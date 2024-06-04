d16412b1ce2ada98158480dd90b89cba759cc334
why does <168974578+why-does@users.noreply.github.com>
2024-06-04 17:37:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class UI(QMainWindow):
         self.pushButton.clicked.connect(self.genld)
         self.pushButton_2.clicked.connect(self.copy)
     def genld(self):
-        # podemb please redo this to use the "loadouts all.txt" file and not this fuckin mess bro its UNIMAGINABLY HORRID
+        # thanks podemb
         if (game := self.game_cbox.currentText()) == "Vanilla":
             with open('loadouts all.txt' if not self.checkBox.isChecked() else 'loadouts nocosre.txt', 'r') as f:
                 loadouts = f.read().split('\n\n')
@@ -43,14 +43,8 @@ class UI(QMainWindow):
 
         if inargs("-debug"):
             print(chosen)
-        # needs fixing lol
-        # if inargs("-saveongen"):
-        #     with open("loadouts.txt", "w") as f:
-        #         for loadout in result:
-        #             f.write(f"{loadout[0]}, {loadout[1]}, {loadout[2]}, {loadout[3]}\n")
-        #     print("saved to loadout.txt")
+        # no it doesnt need fixing, it needs removing (no more -saveongen argument, ya got the fuckin loadouts all.txt file smartypants.)
         
-        # chosen_loadout = random.choice(result)
         self.label.setText(f"Game: {self.game_cbox.currentText()}\nMerc: {self.merc_cbox.currentText()}\nPrimary: {split_l[0]}\nSecondary: {split_l[1]}\nMelee: {split_l[2]}\nPDA: {split_l[3]}\n({len(chosen) - 2} more loadouts available)")
     def copy(self):
         clipboard.copy(f"""This is my automatically generated loadout, done with TF2 Loadout Maker 5000\n{self.label.text()}\nDownload TF2 Loadout Maker 5000 from https://gitlab.com/tf2-based/team-fortress-2-loadout-maker-5000""")
